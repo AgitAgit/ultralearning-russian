984b2eefc6017af007e5ef09487a720c46edf4cb
AmitKubani <amitk@tlvtech.io>
2025-06-30 13:38:02 +0300

    commit

--- V_2025-06-23/server/src/controllers/flashcardController.js
+++ V_2025-06-23/server/src/controllers/flashcardController.js
@@ -1,4 +1,5 @@
 const Flashcard = require("../models/Flashcard.js");
+const Deck = require("../models/Deck.js");
 
 async function addCard(req, res, next) {
     try {
@@ -27,7 +28,75 @@ async function getAllCards(req, res, next) {
     }
 }
 
+async function createDeck(req, res, next) {
+    try {
+        const { name, description } = req.body
+        const deck = new Deck({
+            name,
+            description
+        })
+        const result = await deck.save()
+        res.json(result)
+    } catch (error) {
+        console.log(error)
+        res.json("something went wrong...")
+    }
+}
+
+async function getDeck(req, res, next){
+    try {
+        const { name } = req.params
+        const deck = await Deck.findOne({name}).populate("cards")
+        res.json(deck)
+    } catch (error) {
+        console.log(error)
+        res.json("something went wrong...")
+    }
+}
+
+async function addCardToDeck(req, res, next) {
+    try {
+        const { name, _id, top, bottom, topLanguage, bottomLanguage } = req.body;
+        const deck = await Deck.findOne({ name })
+        if (_id) {
+            deck.cards.push(_id)
+        }
+        else {
+            const card = await Flashcard.findOne({top, bottom, topLanguage, bottomLanguage})
+            deck.cards.push(card._id)
+        }
+        const result = await deck.save()
+        res.json(result)
+    } catch (error) {
+        console.log(error)
+        res.json("something went wrong...")//I could just next(error) and create a catchall error handler
+    }
+}
+
+async function removeCardFromDeck(req, res, next) {
+    try {
+        const { name, _id, top, bottom, topLanguage, bottomLanguage } = req.body;
+        const deck = await Deck.findOne({ name })
+        if (_id) {
+            deck.cards.pull(_id)
+        }
+        else {
+            const card = await Flashcard.findOne({top, bottom, topLanguage, bottomLanguage})
+            deck.cards.pull(card._id)
+        }
+        const result = await deck.save()
+        res.json(result)
+    } catch (error) {
+        console.log(error)
+        res.json("something went wrong...")//I could just next(error) and create a catchall error handler
+    }
+}
+
 module.exports = {
     addCard,
-    getAllCards
+    getAllCards,
+    createDeck,
+    getDeck,
+    addCardToDeck,
+    removeCardFromDeck
 };
--- V_2025-06-23/server/src/routes/flashcards.js
+++ V_2025-06-23/server/src/routes/flashcards.js
@@ -1,10 +1,18 @@
 const express = require('express')
-const { getAllCards, addCard } = require('../controllers/flashcardController')
+const { getAllCards, addCard, createDeck, getDeck, addCardToDeck, removeCardFromDeck } = require('../controllers/flashcardController')
 
 const router = express.Router()
 
 router.get("/", getAllCards)
 
 router.post("/add-one", addCard)
+
+router.post("/create-deck", createDeck);
+
+router.get("/deck/:name", getDeck)
+
+router.post("/add-one-to-deck", addCardToDeck)
+
+router.post("/remove-one-from-deck", removeCardFromDeck);
 
 module.exports = router
